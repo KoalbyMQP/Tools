a6fae02525d439a06a285c4c3b4533cc06ed6db4
Jacob Molnia <jrmolnia@wpi.edu>
2025-08-23 19:36:21 -0400

    Squashed 'RoLint/' content from commit 69f2e04
    git-subtree-dir: RoLint
    git-subtree-split: 69f2e0409bd2fd1a3a81f2a0457797a3d2dc028a

---
+++ examples/bad_code.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <string.h>
+
+char global_buf[100];
+#define MAX_SIZE 100;
+#define SQUARE(x) ((x)*(x))
+
+
+void test() {
+
+    int a;
+    int b = 0;
+    int c = 0, d = 5;
+    int e = 3.14;
+    short s = 1;
+
+
+    b = s;
+
+    
+    char buf[100];
+    gets(buf);                     // 🚨 banned: gets
+    // rolint: ignore
+    strcpy(global_buf, buf);      // 🚨 banned: strcpy
+    sprintf(buf, "Hello %s", buf); // 🚨 banned: sprintf
+
+    b = 4.2;
+    int x = 5;
+    printf("%d\n", x++);          // 🚨 side effect in function arg
+
+    x = (short)b;
+
+    int temp = 42;                
+
+    test();
+
+    // rolint: ignore-block
+    switch (x) {
+    case 1:
+        printf("One\n");  // No break here!
+    case 2:
+        printf("Two\n");
+        break;
+    case 3:
+        printf("Three\n");
+        // fallthrough  //
+    case 4:
+        printf("Four\n");
+        break;
+}
+
+
+    return;
+    temp += 1;                    // 🚨 dead code
+}
+
+int main() {
+    test();
+    return 0;
+}
